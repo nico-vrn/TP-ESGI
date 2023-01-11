1f8c7bea8f2aa3227baa088a12d9cbd8247e046f
nico-vrn <networksgamesni@gmail.com>
2023-01-11 08:48:46 +0100

    exo_2

--- C/TP3-Lefranc/TP3-Lefranc/TP3-Lefranc.c
+++ C/TP3-Lefranc/TP3-Lefranc/TP3-Lefranc.c
@@ -1,4 +1,6 @@
 #include "TP3-Lefranc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef double* PtrTableauDouble;
 
@@ -57,6 +59,73 @@ int C_1() {
     return 0;
 }
 
+
+typedef struct {
+    double* array;
+    unsigned int size;
+} DoubleArray;
+typedef DoubleArray* PtrDoubleArray;
+
+void construct_double_array(PtrDoubleArray* da,unsigned sz) {
+    *da = (PtrDoubleArray) malloc(sizeof(DoubleArray));
+    (*da)->array = (double*) calloc(sz, sizeof(double));
+    (*da)->size = sz;
+}
+
+void print_double_array(const PtrDoubleArray da) {
+    if (da && da->array) {
+        for (unsigned i = 0; i < da->size; i++) {
+            printf("%5.10f ", da->array[i]);
+        }
+        printf("\n");
+    }
+}
+
+void modify_double_array(PtrDoubleArray da, const unsigned idx, const double val) {
+    if (da && da->array && idx < da->size) {
+        da->array[idx] = val;
+    }
+}
+
+void free_double_array(PtrDoubleArray* da) {
+    if (da && *da) {
+        free((*da)->array);
+        free(*da);
+        *da = NULL;
+    }
+}
+
+double get_value_double_array(PtrDoubleArray da, const unsigned idx) {
+    if (da && da->array && idx < da->size) {
+        return da->array[idx];
+    }
+    return 0;
+}
+
+void set_value_double_array(PtrDoubleArray da, const unsigned idx, double val) {
+    if (da && da->array && idx < da->size) {
+        da->array[idx] = val;
+    }
+}
+
+void C_2() {
+    PtrDoubleArray data = NULL;
+    unsigned size=5;
+    construct_double_array(&data, size);
+    print_double_array(data);
+    modify_double_array(data, 2, 3.13589985);
+    print_double_array(data);
+    printf("%5.10f\n", get_value_double_array(data, 2));
+    set_value_double_array(data, 2, 62.1);
+    printf("%5.10f\n", get_value_double_array(data, 2));
+    print_double_array(data);
+    free_double_array(&data);
+    print_double_array(data);
+}
+
+
+
+
 //menu principal
 int main()
 {
@@ -75,7 +144,7 @@ int main()
 			C_1();
 			break;
 		case 2:
-            //no
+            C_2();
 			break;
 		case 3:
             //no
