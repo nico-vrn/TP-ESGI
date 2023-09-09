994f8776cc1e2e3ff117a0c45a043133db4e0254
nico-vrn <networksgamesni@gmail.com>
2023-09-09 12:12:55 +0200

    up

--- C/Cours2/Cours/Cours.c
+++ C/Cours2/Cours/Cours.c
@@ -1,23 +1,144 @@
 #include <stdio.h>
-#include <math.h>
-
-// Fonction pour calculer l'hypoténuse
-double calculer_hypotenuse(double a, double b) {
-    return sqrt(a*a + b*b);
-}
-
+/*
 int main() {
-    double a, b;
+    int capaciteMax = 10;  
+    int notes[capaciteMax];  
+    int nombreDeNotes = 0;  
+    int note;
+    int total = 0;
+    float moyenne;
+    int notesSuperieures = 0;  
 
-    printf("Veuillez entrer le premier cote: ");
-    scanf("%lf", &a); 
+    printf("Saisissez des notes entre 0 et 20. Entrez -1 pour terminer la saisie.\n");
 
-    printf("Veuillez entrer le deuxième cote: ");
-    scanf("%lf", &b); 
+    while (nombreDeNotes < capaciteMax) {
+        printf("Note %d : ", nombreDeNotes + 1);
+        scanf("%d", &note);
 
-    double hypotenuse = calculer_hypotenuse(a, b);
+        if (note == -1) {
+            printf("Saisie terminée.\n");
+            break;  
+        }
 
-    printf("L'hypoténuse du triangle est: %.2lf\n", hypotenuse);
+        if (note < 0 || note > 20) {
+            printf("Erreur : La note doit être entre 0 et 20.\n");
+            continue;  
+        }
+
+        notes[nombreDeNotes] = note;
+        total += note;
+        nombreDeNotes++;
+    }
+
+    if (nombreDeNotes == 0) {
+        printf("Pas de note saisie.\n");
+    } else {
+        moyenne = (float)total / nombreDeNotes;
+        printf("La moyenne des notes saisies est : %.2f\n", moyenne);
+
+        for (int i = 0; i < nombreDeNotes; i++) {
+            if (notes[i] > moyenne) {
+                notesSuperieures++;
+            }
+        }
+
+        printf("Nombre de notes supérieures à la moyenne : %d\n", notesSuperieures);
+    }
 
     return 0;
+}*/
+/*
+#define Max(a,b) (a>b)?a:b
+#define Min(a,b) (a<b)?a:b
+
+int main() {
+    int a;
+    int b;
+    printf("donner un entier"); 
+    scanf("%d", &a);
+    printf("donner un entier");
+    scanf("%d", &b);
+    printf("le max est %d", Max(a,b));
+    printf("Le min est %d", Min(a,b));
+}
+*/
+/*
+struct enregistrement {
+    int num;
+    int quant;
+    float prix;
+};
+
+int main() {
+    struct enregistrement coucou[5];
+    int i;
+    
+    for(i = 0; i < 5; i++) {
+        printf("Donner le numero de l'article : ");
+        scanf("%d", &coucou[i].num);
+        
+        printf("Donner la quantite de l'article : ");
+        scanf("%d", &coucou[i].quant);
+        
+        printf("Donner le prix de l'article : ");
+        scanf("%f", &coucou[i].prix);
+    }
+    
+    for(i=0;i<5;i++){
+        printf("Numero : %d\n", coucou[i].num);
+        printf("Quantite : %d\n", coucou[i].quant);
+        printf("Prix : %f\n", coucou[i].prix);
+    }
+    return 0;
+}*/
+/*
+
+struct enregistrement {
+    int num;
+    int quant;
+    float prix;
+};
+
+int main(){
+    struct enregistrement *pt_struct, *pt_courant;
+
+    pt_struct = malloc(5*sizeof(struct enregistrement));
+    
+    pt_courant = pt_struct;
+
+    for(;pt_courant < pt_struct + 5; pt_courant++){
+        printf("Donner le numero de l'article : ");
+        scanf("%d", &pt_courant->num);
+        
+        printf("Donner la quantite de l'article : ");
+        scanf("%d", &pt_courant->quant);
+        
+        printf("Donner le prix de l'article : ");
+        scanf("%f", &pt_courant->prix);
+    }
+
+    pt_courant = pt_struct;
+
+    for(;pt_courant<pt_struct+5;pt_courant++){
+        printf("Numero : %d\n", pt_courant->num);
+        printf("Quantite : %d\n", pt_courant->quant);
+        printf("Prix : %f\n", pt_courant->prix);
+    }
+
+    free(pt_struct);
+    return 0;
+}*/
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *head = NULL;
+
+void insert(int new_data) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = head;
+    head = new_node;
 }
---
+++ SQL_Lefranc.sql
@@ -0,0 +1,99 @@
+﻿Travail Lefranc Nicolas 
+
+CREATE TABLE "Client" (
+    "id_cli" int   NOT NULL,
+    "nom" int   NOT NULL,
+    "ville" int   NOT NULL,
+    "prenom" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_Client" PRIMARY KEY (
+        "id_cli"
+     )
+);
+
+CREATE TABLE "Product" (
+    "ProductID" int   NOT NULL,
+    "Name" varchar(200)   NOT NULL,
+    "Price" money   NOT NULL,
+    "stock" int   NOT NULL,
+    "marque" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_Product" PRIMARY KEY (
+        "ProductID"
+     ),
+    CONSTRAINT "uc_Product_Name" UNIQUE (
+        "Name"
+    )
+);
+
+CREATE TABLE "Vente" (
+    "id_vente" int   NOT NULL,
+    "id_cli" int   NOT NULL,
+    "id_pro" int   NOT NULL,
+    "date" date   NOT NULL,
+    "quantite" int   NOT NULL,
+    CONSTRAINT "pk_Vente" PRIMARY KEY (
+        "id_vente"
+     )
+);
+
+ALTER TABLE "Vente" ADD CONSTRAINT "fk_Vente_id_cli" FOREIGN KEY("id_cli")
+REFERENCES "Client" ("id_cli");
+
+ALTER TABLE "Vente" ADD CONSTRAINT "fk_Vente_id_pro" FOREIGN KEY("id_pro")
+REFERENCES "Product" ("ProductID");
+
+-- Partie 1 : 
+-- Ajouter une colonne marque dans la table produit
+ALTER TABLE "Product" ADD COLUMN "marque" varchar(200);
+
+-- Ajouter une colonne prenom dans la table client
+ALTER TABLE "Client" ADD COLUMN "prenom" varchar(200);
+
+-- Diminuer le prix de 5% dans la table produit
+UPDATE "Product" SET "Price" = "Price" * 0.95;
+
+-- Lister les clients dont le nom comporte la lettre L en 3ième position
+SELECT * FROM "Client" WHERE SUBSTRING("nom", 3, 1) = 'L';
+
+-- Lister les produits dont le prix est compris entre 500€ et 1100€
+SELECT * FROM "Product" WHERE "Price" BETWEEN 500 AND 1100;
+
+-- Lister les produits de marque IBM, Apple ou Dec
+SELECT * FROM "Product" WHERE "marque" IN ('IBM', 'Apple', 'Dec');
+
+-- Lister les produits de marque IBM dont le prix est inférieur à 1000€
+SELECT * FROM "Product" WHERE "marque" = 'IBM' AND "Price" < 1000;
+
+
+-- Partie 2 : 
+
+-- Donner les références et les noms des produits vendus
+SELECT DISTINCT "ProductID", "Name" FROM "Product" INNER JOIN "Vente" ON "Product"."ProductID" = "Vente"."id_pro";
+
+-- Donner les noms des clients qui ont acheté le produit ‘Auto'
+SELECT DISTINCT "nom" FROM "Client" INNER JOIN "Vente" ON "Client"."id_cli" = "Vente"."id_cli" INNER JOIN "Product" ON "Vente"."id_pro" = "Product"."ProductID" WHERE "Product"."Name" = 'Auto';
+
+-- Donner les noms des produits qui n'ont pas été acheté trier par le nom du produit
+SELECT "Name" FROM "Product" WHERE "ProductID" NOT IN (SELECT "id_pro" FROM "Vente") ORDER BY "Name";
+
+-- Donner le nombre total de clients ayant acheté des produits
+SELECT COUNT(DISTINCT "id_cli") FROM "Vente";
+
+-- Donner le nombre total de produit 'Auto' vendus
+SELECT SUM("quantite") FROM "Vente" INNER JOIN "Product" ON "Vente"."id_pro" = "Product"."ProductID" WHERE "Product"."Name" = 'Auto';
+
+-- Donner les noms des produits moins chers que la moyenne des prix de tous les produits
+SELECT "Name" FROM "Product" WHERE "Price" < (SELECT AVG("Price") FROM "Product");
+
+-- Donner pour chaque référence de produit la quantité totale vendue
+SELECT "id_pro", SUM("quantite") FROM "Vente" GROUP BY "id_pro";
+
+-- Donner les noms des marques dont le prix moyen des produits est < 5000€
+SELECT "marque" FROM "Product" GROUP BY "marque" HAVING AVG("Price") < 5000;
+
+-- Donner les noms des produits achetés en qte > 10 par plus de 50 clients
+SELECT "Name" FROM "Product" WHERE "ProductID" IN (SELECT "id_pro" FROM "Vente" GROUP BY "id_pro" HAVING COUNT(DISTINCT "id_cli") > 50 AND SUM("quantite") > 10);
+
+-- Augmenter de 30% les prix des produits achetés par des clients de Nice
+UPDATE "Product" SET "Price" = "Price" * 1.3 WHERE "ProductID" IN (SELECT DISTINCT "id_pro" FROM "Vente" INNER JOIN "Client" ON "Vente"."id_cli" = "Client"."id_cli" WHERE "Client"."ville" = 'Nice');
+
+
