5d958dc9b140c40c71940de75ddfadd2c2f12686
nico-vrn <networksgamesni@gmail.com>
2024-05-16 11:40:53 +0200

    networkscan

--- secu_python/ch23_box/scan_network.py
+++ secu_python/ch23_box/scan_network.py
@@ -1,21 +1,29 @@
-import nmap
+from scapy.all import ARP, Ether, srp
 
-def network_scan(target_network):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=target_network, arguments='-sn')  # Ping scan
-    hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-    return hosts_list
+def network_scan(target_ip):
+    arp = ARP(pdst=target_ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    clients = []
+
+    for sent, received in result:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+    return clients
 
 def main():
-    target_network = "192.168.56.0/24" 
-    print(f"Scanning network {target_network}...")
+    target_ip = "192.168.56.0/24" 
+    print(f"Scanning network {target_ip}...")
 
-    hosts = network_scan(target_network)
+    clients = network_scan(target_ip)
 
     print("Available devices in the network:")
-    print("IP" + " "*18 + "State")
-    for host, status in hosts:
-        print("{:16}    {}".format(host, status))
+    print("IP" + " "*18 + "MAC")
+    for client in clients:
+        print("{:16}    {}".format(client['ip'], client['mac']))
 
 if __name__ == "__main__":
     main()
