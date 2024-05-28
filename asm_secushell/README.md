bbf6fa55a595dcd5fdde9c2ca577b4075b3d74e8
nico-vrn <networksgamesni@gmail.com>
2024-05-28 22:39:25 +0200

    Final => fonctionnel

--- asm_secushell/README.md
+++ asm_secushell/README.md
@@ -9,26 +9,34 @@
 ## asm_md5
 Le fichier "MD5.s" est censé faire un hash md5 d'un fichier. 
 
-Le fichier à hash est modifiable ligne 13 du fichier, par défaut c'est "fichier.txt".
+Le fichier à hash est modifiable ligne 9 du fichier, par défaut c'est "fichier.txt".
 
 En revanche il ne fonctionne pas bien. 
 
 Il affiche le contenu du fichier et un hash mais le hash est faux et je n'ai pas réussi à corriger le problème.
+Par contre le nombre de carractère Hexadécimal est correct.
 
 ### Compilation 
+Pour compiler le code correctement et le rendre exécutable : 
+```sh 
 nasm -f elf64 MD5.s 
-
+```
+```sh
 ld -o MD5 MD5.o -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lcrypto
-
+```
+```sh
 ./MD5
+```
 
 ### Sortie
-Hello, World!
+```sh
+L'assembleur c'est l'enfer.
 
-11aa1144ff9900ff44ee00bb2255cc88
+2c3948cacc3fe672c7d9583abe93beec
+```
 
 ### Erreur 
-Après vérification, le hash est censé être : bea8252ff4e80f41719ea13cdf007273
+Après vérification, le hash est censé être : e165f7c4925635da0d0ba02f232040a7
 
 Donc le fichier ne fonctionne pas.
 
@@ -46,11 +54,16 @@ Ce code en assembleur appel la fonction C et lui donne en objet le fichier à ha
 Le fichier à hash est modifiable ligne 8 du fichier, par défaut c'est "fichier.txt".
 
 ### Compilation 
+Pour compiler le code correctement et le rendre exécutable : 
+```sh
 gcc -c md5.c -o md5hash.o
-
+```
+```sh
 nasm -f elf64 -g md5c.asm -o assembleur.o
-
+```
+```sh
 gcc -o programme_final assembleur.o md5hash.o -lcrypto -nostartfiles -no-pie
+```
 
 ### Sortie 
-MD5 hash of "fichier.txt": bea8252ff4e80f41719ea13cdf007273
+MD5 hash of "fichier.txt": 65a8e27d8879283831b664bd8b7f0ad4
--- asm_secushell/asm_md5/MD5.s
+++ asm_secushell/asm_md5/MD5.s
@@ -1,150 +1,126 @@
+; Programme asm qui hash en MD5
 extern MD5_Init
 extern MD5_Update
 extern MD5_Final
 
-%define BUFFER_SIZE 1024
+%define BUFFER_SIZE 1024 ; défini la taille du buffer
 
 section .data
-    file_path   db 'fichier.txt', 0    ; Chemin du fichier
-    md5_res     times 16 db 0          ; Buffer pour stocker le résultat du hachage MD5
-    buffer      times BUFFER_SIZE db 0 ; Buffer pour lire le fichier
-    md5_ctx     times 104 db 0         
-    hex_digits  db '0123456789abcdef'  ; Caractères hexadécimaux pour la conversion
-    newline     db 0xa                 ; Caractère de nouvelle ligne
+    file_path db 'fichier.txt', 0 ;le fichier à Hash
+    md5_res times 16 db 0
+    buffer times BUFFER_SIZE db 0
+    md5_ctx times 104 db 0
+    hex_digits db '0123456789abcdef', 0
+    newline db 0xa, 0             
 
 section .bss
-    hex_buffer  resb 33                ; Buffer pour stocker le hachage MD5
+    hex_buffer resb 33
 
 section .text
     global _start
 
 _start:
-    mov rdi, md5_ctx
-    call align_buffer
-
-    ; Initialiser le contexte MD5.
-    lea rdi, [md5_ctx]       ; pointeur vers md5_ctx
+    ; Initialisation 
+    lea rdi, [md5_ctx]
     call MD5_Init
 
-    ; Ouvrir le fichier en lecture seule.
-    mov rax, 2               
-    mov rdi, file_path       ; chemin du fichier
-    xor rsi, rsi             
+    ; Ouvrir le fichier
+    mov rax, 2              
+    lea rdi, [file_path]    
+    xor rsi, rsi            ; mode lecture
     syscall
-    cmp rax, 0               ; Vérifier si l'appel système a réussi
-    jl  error_exit           
-    mov rbx, rax             ; Sauvegarder le descripteur de fichier dans rbx
+    test rax, rax
+    js _error               ; si erreur
+    mov rbx, rax            
 
-read_loop:
-    ; Lire le contenu du fichier par blocs.
-    mov rax, 0               
-    mov rdi, rbx             
-    mov rsi, buffer          
-    mov rdx, BUFFER_SIZE     
+_read:
+    ; Lire le fichier
+    mov rax, 0              
+    mov rdi, rbx            
+    lea rsi, [buffer]       ; buffer
+    mov rdx, BUFFER_SIZE    
     syscall
-    cmp rax, 0               ; Vérifier si la fin du fichier a été atteinte ou une erreur s'est produite
-    jle md5_end              ; Si rax est 0 ou négatif, fin du fichier ou erreur
+    test rax, rax
+    jle _done_reading       
 
-    ; Afficher le contenu du buffer
-    mov rdx, rax             
-    mov rax, 1               
-    mov rdi, 1               ; Sortie standard (écran)
-    mov rsi, buffer          
+    ; Afficher le contenu du fichier
+    mov rdx, rax            
+    mov rax, 1             
+    mov rdi, 1              
+    lea rsi, [buffer]      
     syscall
 
-    ; Mettre à jour le contexte MD5 avec le bloc lu.
-    mov rdi, md5_ctx         ; contexte MD5
-    mov rsi, buffer          ; données à hacher
-    mov rdx, rax             ; nombre d'octets lus
+    ; Mettre à jour le hash MD5
+    lea rdi, [md5_ctx]
+    lea rsi, [buffer]
+    mov rdx, rax
     call MD5_Update
-    jmp read_loop
 
-md5_end:
-    ; Finaliser le hachage MD5.
-    mov rdi, md5_ctx         
-    lea rsi, [md5_res]       
+    jmp _read
+
+_done_reading:
+    ; Finaliser le hash MD5
+    lea rdi, [md5_ctx]
+    lea rsi, [md5_res]
     call MD5_Final
 
-    ; Afficher une nouvelle ligne.
-    mov rax, 1               
-    mov rdi, 1               ; Sortie standard (écran)
-    mov rsi, newline         ; Pointeur vers le caractère de nouvelle ligne
-    mov rdx, 1               ; Nombre d'octets à afficher (1 caractère)
+    ; Print une nouvelle ligne
+    mov rax, 1              
+    mov rdi, 1              
+    lea rsi, [newline]
+    mov rdx, 1
     syscall
 
-    ; Afficher le résultat du hachage MD5 sous forme hexadécimale.
-    lea rsi, [md5_res]       ; Pointeur vers le résultat du hachage MD5
-    lea rdi, [hex_buffer]    
-    call print_md5_hex
+    ; Convertir le hash MD5 en hexadécimal
+    lea rsi, [md5_res]
+    lea rdi, [hex_buffer]
+    call to_hex
 
-    ; Afficher la représentation hexadécimale du hachage MD5.
-    mov rax, 1               
-    mov rdi, 1               
-    mov rsi, hex_buffer      
-    mov rdx, 32              
+    ; Afficher le hash hexadecimal MD5 
+    mov rax, 1              
+    mov rdi, 1              
+    lea rsi, [hex_buffer]
+    mov rdx, 32
     syscall
 
-    ; Afficher une nouvelle ligne.
-    mov rax, 1               
-    mov rdi, 1               ; Sortie standard (écran)
-    mov rsi, newline         
-    mov rdx, 1               
+    ; Print une nouvelle ligne
+    mov rax, 1              
+    mov rdi, 1              
+    lea rsi, [newline]
+    mov rdx, 1
     syscall
 
-    ; Fermer le fichier
-    mov rax, 3               
-    mov rdi, rbx             
-    syscall
-
-    ; Sortie du programme
+    ; Exit
     mov rax, 60             
-    xor rdi, rdi             
+    xor rdi, rdi            
     syscall
 
-print_md5_hex:
-    ; Fonction pour convertir le résultat du hachage MD5 en représentation hexadécimale.
-    ; rsi : pointeur vers le résultat du hachage MD5
-    push rax
+to_hex:
+    ; rsi = input buffer (MD5 hash)
+    ; rdi = output buffer (hexadecimal string)
     push rbx
     push rcx
     xor rax, rax
-    xor rbx, rbx
 
-print_md5_hex_loop:
+convert_loop:
     mov bl, byte [rsi + rax]
     shr bl, 4
-    mov cl, hex_digits[rbx] 
+    mov cl, [hex_digits + rbx]
     mov byte [rdi + rax * 2], cl
-    and bl, 0fh
-    mov cl, hex_digits[rbx] 
+    mov bl, byte [rsi + rax]
+    and bl, 0xF
+    mov cl, [hex_digits + rbx]
     mov byte [rdi + rax * 2 + 1], cl
     inc rax
     cmp rax, 16
-    jne print_md5_hex_loop
+    jne convert_loop
 
-    mov byte [rdi + 32], 0   
-
+    mov byte [rdi + rax * 2], 0
     pop rcx
     pop rbx
-    pop rax
     ret
 
-align_buffer:
-    ; Fonction pour aligner un tampon sur une frontière de 8 octets.
-    push rbx
-    mov rbx, rdi
-    and rbx, 0x7             
-    jz  aligned              ; Si rbx est nul, le tampon est déjà aligné
-    add rdi, 8
-    sub rdi, rbx             ; Ajuster le pointeur du tampon
-
-aligned:
-    mov rax, rdi
-    pop rbx
-    ret
-
-error_exit:
-    ; Code pour gérer les erreurs (fermeture de fichiers, libération de la mémoire, etc.)
-    mov rax, 60              ; Appel système exit
-    mov rdi, 1               ; Code de sortie 1 (erreur)
+_error:
+    mov rax, 60             
+    mov rdi, 1              ; sortie propre
     syscall
