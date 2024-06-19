64ed95502d0018d5cb6b48380aba0511a8afaeb0
nico-vrn <networksgamesni@gmail.com>
2024-06-19 09:08:35 +0200

    scan_network

--- secu_python/ch23_box/scan_network.py
+++ secu_python/ch23_box/scan_network.py
@@ -1,17 +1,43 @@
-import subprocess
+from ping3 import ping
+import concurrent.futures
 
-def run_nmap_scan():
+def ping_ip(ip):
+    """
+    Ping une adresse IP pour vérifier si elle est active.
+    """
     try:
-        command = ['sudo', 'nmap', '-sn', '192.168.56.0/24']
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        response = ping(ip, timeout=1)
+        if response:
+            return ip
+    except Exception as e:
+        pass
+    return None
 
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
-        print(e.output)
-
-def main():
-    run_nmap_scan()
+def scan_network(base_ip, start, end):
+    """
+    Scanne les adresses IP dans la plage donnée.
+    """
+    active_ips = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = {executor.submit(ping_ip, f"{base_ip}.{i}"): i for i in range(start, end + 1)}
+        for future in concurrent.futures.as_completed(futures):
+            ip = future.result()
+            if ip:
+                active_ips.append(ip)
+    return active_ips
 
 if __name__ == "__main__":
-    main()
+    base_ip = input("Entrez la base de l'adresse IP (par ex. 192.168.1): ")
+    start_ip = int(input("Entrez le début de la plage d'IP: "))
+    end_ip = int(input("Entrez la fin de la plage d'IP: "))
+
+    print(f"Scanning the network {base_ip}.{start_ip} to {base_ip}.{end_ip}...")
+
+    active_ips = scan_network(base_ip, start_ip, end_ip)
+
+    if active_ips:
+        print(f"Adresses IP actives trouvées sur le réseau {base_ip}:")
+        for ip in active_ips:
+            print(ip)
+    else:
+        print(f"Aucune adresse IP active trouvée sur le réseau {base_ip} dans la plage spécifiée.")
