1c6dd0d4aa9e805c1e8e7d44b2fa4cd0db561edb
nico-vrn <networksgamesni@gmail.com>
2024-06-27 22:04:37 +0200

    fonctionne

--- asm_secushell/compiler/compiler_C.py
+++ asm_secushell/compiler/compiler_C.py
@@ -1,42 +1,39 @@
-import llvmlite.binding as llvm
 import os
 import sys
 import subprocess
 
 class SimpleCCompiler:
     def __init__(self):
-        self.module = None
-        self.engine = self.create_execution_engine()
+        pass
 
-    def create_execution_engine(self):
-        llvm.initialize()
-        llvm.initialize_native_target()
-        llvm.initialize_native_asmprinter()
-        target = llvm.Target.from_default_triple()
-        target_machine = target.create_target_machine()
-        backing_mod = llvm.parse_assembly("")
-        engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
-        return engine
+    def preprocess(self, source_file):
+        preprocessed_file = "preprocessed.c"
+        with open(source_file, 'r') as f:
+            code = f.read()
+        
+        # Ajouter l'inclusion de stdio.h si elle n'est pas présente
+        if '#include <stdio.h>' not in code:
+            code = '#include <stdio.h>\n' + code
 
-    def compile_ir(self, llvm_ir):
-        mod = llvm.parse_assembly(llvm_ir)
-        mod.verify()
-        self.engine.add_module(mod)
-        self.engine.finalize_object()
-        self.engine.run_static_constructors()
-        return mod
-
-    def save_object_file(self, llvm_module, output_file):
-        target = llvm.Target.from_default_triple()
-        target_machine = target.create_target_machine()
-        with open(output_file, 'wb') as f:
-            f.write(target_machine.emit_object(llvm_module))
+        with open(preprocessed_file, 'w') as f:
+            f.write(code)
+        
+        return preprocessed_file
 
     def compile(self, source_file):
+        preprocessed_file = self.preprocess(source_file)
+        
+        # Compiler le fichier source pré-traité en fichier objet
         obj_file = "output.o"
-        subprocess.run(["gcc", "-c", source_file, "-o", obj_file], check=True)
-        os.system(f"gcc -o output {obj_file}")
+        subprocess.run(["gcc", "-c", preprocessed_file, "-o", obj_file], check=True)
+        
+        # Créer un exécutable statique
+        subprocess.run(["gcc", "-static", "-o", "output", obj_file], check=True)
+        
         print(f"Compilation réussie. Exécutable créé: output")
+        
+        # Nettoyer le fichier pré-traité
+        os.remove(preprocessed_file)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
